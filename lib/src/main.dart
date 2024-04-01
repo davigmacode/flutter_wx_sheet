28a862a477bdd760275e7b420854811385b2e559
Irfan Vigma Taufik <davigmacode@gmail.com>
2024-04-01 08:32:41 +0700

    Adjust entry point

--- lib/src/main.dart
+++
@@ -1,6 +0,0 @@
-export 'style.dart';
-export 'theme_data.dart';
-export 'theme_preset.dart';
-export 'theme.dart';
-export 'types.dart';
-export 'widget.dart';
--- lib/wx_sheet.dart
+++ lib/wx_sheet.dart
@@ -1,7 +1,9 @@
 library wx_sheet;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/style.dart';
+export 'src/theme_data.dart';
+export 'src/theme_preset.dart';
+export 'src/theme.dart';
+export 'src/types.dart';
+export 'src/variant.dart';
+export 'src/widget.dart';
