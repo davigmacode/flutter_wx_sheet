e2851192fcb2ff3ce85abb8a85a1fe20f67cd7c4
Irfan Vigma Taufik <davigmacode@gmail.com>
2024-07-12 10:35:07 +0700

    Updated test

--- test/wx_sheet_shape_test.dart
+++ test/wx_sheet_shape_test.dart
@@ -3,24 +3,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:wx_sheet/wx_sheet.dart';
 
 void main() {
-  testWidgets('WxStaticSheet.square() constructor',
-      (WidgetTester tester) async {
-    const widget = WxStaticSheet.square(
-      size: 100,
-      child: Text('Hello, world!'),
-    );
-
-    await tester.pumpWidget(const MaterialApp(
-      home: Scaffold(
-        body: widget,
-      ),
-    ));
-
-    expect(find.text('Hello, world!'), findsOneWidget);
-    expect(tester.getSize(find.byType(WxStaticSheet)).width, 100);
-    expect(tester.getSize(find.byType(WxStaticSheet)).height, 100);
-  });
-
   testWidgets('WxStaticSheet.circle() constructor',
       (WidgetTester tester) async {
     const widget = WxStaticSheet.circle(
